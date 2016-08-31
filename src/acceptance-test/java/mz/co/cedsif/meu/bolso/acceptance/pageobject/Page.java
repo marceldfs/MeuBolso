391db0a1d681bb659e59dc28fa0250085add1927
gracio <gracio.mambo@gmail.com>
2016-08-31 09:59:28 +0200

    ssj

--- src/acceptance-test/java/mz/co/cedsif/meu/bolso/acceptance/pageobject/Page.java
+++ src/acceptance-test/java/mz/co/cedsif/meu/bolso/acceptance/pageobject/Page.java
@@ -1,7 +1,6 @@
 package mz.co.cedsif.meu.bolso.acceptance.pageobject;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Page {
