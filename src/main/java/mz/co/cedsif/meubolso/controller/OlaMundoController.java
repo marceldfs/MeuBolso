2dc3251e1ae51a4ebb24f6c00374752fd68b70e6
DiCR <dicr@gma.com>
2016-09-01 10:17:21 +0200

    Fazendo o mapeamento para a pagina Novo Gasto

--- src/main/java/mz/co/cedsif/meubolso/controller/OlaMundoController.java
+++ src/main/java/mz/co/cedsif/meubolso/controller/OlaMundoController.java
@@ -5,10 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OlaMundoController {
-	
+
 	@RequestMapping("/olaMundoSpring")
-	public String execute(){
+	public String execute() {
 		System.out.println("Executando a logica com o Spring MVC");
 		return "ola";
-}
+	}
+
+	@RequestMapping("/novogasto")
+	public String mostrarPaginaRegistarGastos() {
+		System.out.println("Executando a logica com o Spring MVC");
+		return "gastos";
+	}
 }
