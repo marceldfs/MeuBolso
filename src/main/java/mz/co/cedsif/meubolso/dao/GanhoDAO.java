9e9f0f29b42fb71a6d896752d508eb25a2901fce
tedfernandess <ted.fernandess@gmail.com>
2016-09-01 11:02:51 +0200

    refatorando o GanhoDAO

--- src/main/java/mz/co/cedsif/meubolso/dao/GanhoDAO.java
+++ src/main/java/mz/co/cedsif/meubolso/dao/GanhoDAO.java
@@ -1,14 +1,27 @@
 package mz.co.cedsif.meubolso.dao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import mz.co.cedsif.meubolso.model.Ganho;
+
 public class GanhoDAO {
 
-	public void inserir() {
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("ganho");
-		EntityManager manager = factory.createEntityManager();
+	private EntityManager manager;
 
-		manager.getTransaction().begin();
-		manager.persist(ganho);
-		manager.close();
+	public GanhoDAO(EntityManager manager) {
+		this.manager = manager;
+	}
+
+	public void inserir(Ganho ganho) {
+
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("ganho");
+		this.manager = factory.createEntityManager();
+
+		this.manager.getTransaction().begin();
+		this.manager.persist(ganho);
+		this.manager.getTransaction().commit();
 	}
 
 }
