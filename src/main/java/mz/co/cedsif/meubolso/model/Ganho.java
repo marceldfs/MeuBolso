c96b2d6349252fdc33c3652e21c9106ccf1bd2ef
tedfernandess <ted.fernandess@gmail.com>
2016-09-01 11:04:41 +0200

    Alterando o tipo de dados da variavel dada da classe Ganho.

--- src/main/java/mz/co/cedsif/meubolso/model/Ganho.java
+++ src/main/java/mz/co/cedsif/meubolso/model/Ganho.java
@@ -2,11 +2,13 @@ package mz.co.cedsif.meubolso.model;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+@Entity
 public class Ganho {
 
 	@Id
@@ -14,12 +16,27 @@ public class Ganho {
 	private Long id;
 
 	@Temporal(TemporalType.DATE)
-	private LocalDate data;
+	private Date data;
 
 	private String descricao;
 	private double valor;
 
-	private String tipo;
+	private TipoGanho tipo;
+
+	public Ganho() {
+
+	}
+
+	public Ganho(Date data, String descricao, TipoGanho tipo, double valor) {
+		this.data = data;
+		this.descricao = descricao;
+		this.tipo = tipo;
+		this.valor = valor;
+	}
+
+	public Long getId() {
+		return id;
+	}
 
 	public Date getData() {
 		return data;
@@ -29,19 +46,19 @@ public class Ganho {
 		this.data = data;
 	}
 
-	public String getDescricao() {
+	public String getDescrição() {
 		return descricao;
 	}
 
-	public void setDescricao(String descricao) {
+	public void setDescrição(String descricao) {
 		this.descricao = descricao;
 	}
 
-	public String getTipo() {
+	public TipoGanho getTipo() {
 		return tipo;
 	}
 
-	public void setTipo(String tipo) {
+	public void setTipo(TipoGanho tipo) {
 		this.tipo = tipo;
 	}
 
@@ -51,9 +68,6 @@ public class Ganho {
 
 	public void setValor(Double valor) {
 		this.valor = valor;
-	}
-
-	public Ganho() {
 	}
 
 }
