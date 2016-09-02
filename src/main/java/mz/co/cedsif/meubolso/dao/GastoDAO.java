d88a798625f479d8e301e70c7b455df36f0a976b
Yuri Wingester <yuri2jwingester@gmail.com>
2016-09-02 10:25:20 +0200

    fazer o teste passar de forma mais simples

--- src/main/java/mz/co/cedsif/meubolso/dao/GastoDAO.java
+++ src/main/java/mz/co/cedsif/meubolso/dao/GastoDAO.java
@@ -1,11 +1,25 @@
 package mz.co.cedsif.meubolso.dao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import mz.co.cedsif.meubolso.model.Gasto;
 
 public class GastoDAO {
+	
+	private EntityManager manager;
+	
 
 	public void inserir(Gasto gasto) {
-		// TODO Auto-generated method stub
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("gasto");
+		this.manager = factory.createEntityManager();
+
+		this.manager.getTransaction().begin();
+		this.manager.persist(gasto);
+		this.manager.getTransaction().commit();
+		this.manager.close();
 		
 	}
 
