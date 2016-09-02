b64d56a90f44da26034049aeec25503bcfafef9d
Yuri Wingester <yuri2jwingester@gmail.com>
2016-09-02 10:22:13 +0200

    Adicao do metodo para testar a insercao do Gasto

---
+++ src/main/java/mz/co/cedsif/meubolso/dao/GastoDAO.java
@@ -0,0 +1,12 @@
+package mz.co.cedsif.meubolso.dao;
+
+import mz.co.cedsif.meubolso.model.Gasto;
+
+public class GastoDAO {
+
+	public void inserir(Gasto gasto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/test/java/mz/co/cedsif/meubolso/dao/GastoDAOTest.java
+++ src/test/java/mz/co/cedsif/meubolso/dao/GastoDAOTest.java
@@ -1,5 +1,13 @@
 package mz.co.cedsif.meubolso.dao;
 
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+import mz.co.cedsif.meubolso.model.Gasto;
+
 /**
  * Classe de teste para o GASTO DAO
  * 
@@ -8,6 +16,22 @@ package mz.co.cedsif.meubolso.dao;
 public class GastoDAOTest 
 {
 	
+	@Test
+	public void deveInserirGasto() {
+
+		Date data = new Date();
+		String descricao = "ALMOCO";
+		double valor = 500.0;
+		
+		Gasto gasto = new Gasto();
+		gasto.setData(data);
+		gasto.setDescricao(descricao);
+		gasto.setValor(valor);
+		
+		new GastoDAO().inserir(gasto);	
+
+		assertNotNull(gasto.getId());
+	}
 	
 	
 	
