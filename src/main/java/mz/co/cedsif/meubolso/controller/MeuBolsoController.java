7c87daa1c4b5609c6220bbcd575e767df1160102
tedfernandess <ted.fernandess@gmail.com>
2016-09-02 12:17:34 +0200

    Fazendo o mapeamento para adicionar o ganho

--- src/main/java/mz/co/cedsif/meubolso/controller/MeuBolsoController.java
+++ src/main/java/mz/co/cedsif/meubolso/controller/MeuBolsoController.java
@@ -1,10 +1,24 @@
 package mz.co.cedsif.meubolso.controller;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import mz.co.cedsif.meubolso.dao.GastoDAO;
+import mz.co.cedsif.meubolso.model.Gasto;
+
 @Controller
 public class MeuBolsoController {
+	private GastoDAO gastoDAO;
+
+	public MeuBolsoController() {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("gasto");
+		EntityManager manager = factory.createEntityManager();
+		this.gastoDAO = new GastoDAO(manager);
+	}
 
 	@RequestMapping("/")
 	public String mostrarPaginaInicial() {
@@ -22,5 +36,11 @@ public class MeuBolsoController {
 	public String mostrarPaginaRegistarGanhos() {
 		System.out.println("Executando a logica com o Spring MVC");
 		return "gastos";
+	}
+
+	@RequestMapping("/adicionaGasto")
+	public String adicionaGastos(Gasto gasto) {
+		gastoDAO.inserir(gasto);
+		return "index";
 	}
 }
